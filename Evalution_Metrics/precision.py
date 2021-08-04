0f9d5d41a15083aa84b2fbe31c8c8b2d666b9ca1
Tarequzzaman <tareqcse12@gmail.com>
2021-08-04 23:41:37 +0600

    precision added

---
+++ Evalution_Metrics/precision.py
@@ -0,0 +1,22 @@
+
+from BasicMetrics import true_positive, false_positive
+
+
+def precision(y_true, y_pred) -> float: 
+
+    TP = true_positive(y_true, y_pred)
+    print(TP)
+    FP = false_positive(y_true, y_pred)
+
+    precision = TP / (TP+FP)
+    print(precision)
+
+    return precision
+
+
+
+
+l1 = [0,1,1,1,0,0,0,1]
+l2 = [0,1,0,1,0,1,0,0]
+
+print(precision(l1, l2))
